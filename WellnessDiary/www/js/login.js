ac059d5607b69b9ec04e0787c1363815b3082efb
Yating L <yl723811@ohio.edu>
2014-09-24 18:26:05 -0400

    Add an initial login html filefor Facebook login

---
+++ WellnessDiary/www/js/login.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yatingliu on 9/23/14.
+ */
+<script language="javascript">
+FB.login(function(response) {
+    if (response.authResponse) {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+    console.log('Good to see you, ' + response.name + '.');
+    });
+    } else {
+    console.log('User cancelled login or did not fully authorize.');
+    }
+    });
+</script>
+
